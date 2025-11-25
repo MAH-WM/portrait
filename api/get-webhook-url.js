90b9be7edf7bea07e18b48f91af6d1f272e08820
MAH-WM <mh@watchmedier.dk>
2025-11-25 14:02:38 +0100

    Delete api directory

--- api/get-webhook-url.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Content-Type', 'application/json');
-  
-  res.status(200).json({
-    webhookUrl: process.env.ZAPIER_WEBHOOK_URL
-  });
-}
